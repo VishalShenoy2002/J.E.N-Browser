ee7066d5628bd4c2cda94cff6e53e9f76071e504
Vishal Shenoy <61897464+VishalShenoy2002@users.noreply.github.com>
2021-05-03 16:01:48 +0530

    Deleted git_transfer.py as it was not needed.

--- git_transfer.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for file in os.listdir(os.getcwd()):
-    if os.path.isfile(os.getcwd()+'\\{}'.format(file)):
-        if 'git' in file.lower() or file.endswith('.md')==False:
-            os.system('git add {}'.format(file))
